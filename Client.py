5c3bf1475b889edbc5c3a5df4f17f6155d3b2f32
stewajos <35934674+stewajos@users.noreply.github.com>
2020-02-23 15:58:50 -0500

    Update Client.py
    Completed client server

--- Client.py
+++ Client.py
@@ -8,81 +8,84 @@ PORT = 0  # The port used by the server
 
 def client():
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    the_socket = socket.socket()
 
-        # s.sendall(b'Connection works!')
 
-        def connector(IP, socketPort):
-            global HOST
-            global PORT
-            HOST = IP
-            PORT = socketPort
-            s.connect((HOST, PORT))
+    # this method is intended to recieve a list of files in the directory the server is currently in
+    def list_receiver():
+        the_socket.send(b'list')
+        print("waiting to recieve")
+        data = the_socket.recv(1024).decode()
+        print(data)
+        print("")
 
-        def list():
-            s.sendall(b'list')
-            data = headerDecoder()
-            data = s.recv(1024).decode()
-            print(data)
+    #
+    def retrieve(filename):
+        the_socket.send(b'retrieve')
+        # the_socket.recv(1024).decode()
+        the_socket.send(filename.encode())
+        dataChunk = the_socket.recv(4096)
+        with open(filename, 'wb+') as f:
+            f.write(dataChunk)
 
-        def retrieve(filename):
-            print("retrieve")
 
-        def store(filename):
-            print("store")
+        print(filename + " received")
 
-        def quitty():
-            print("quit")
+    def store(filename):
+        the_socket.send(b'store')
+        file = open(filename, "rb")
+        the_socket.send(filename.encode())
+        the_socket.sendall(file.read())
+        print("File sent. ")
 
-        def headerDecoder():
-            varLength = s.recv(2)
-            totalLength = s.recv(varLength)
-            data = s.recv(totalLength).decode()
-            return data
+    def quitTransaction():
+        the_socket.send(b'Reached quitTransaction()')
+        the_socket.send(b'quit')
+        the_socket.close()
 
-        def headerEncoder(file):
-            fileSize = os.path.getsize(file)
-            fileSizeSize = math.ceil(fileSize.bit_length() / 8)
-            return (fileSize, fileSizeSize)
+    menu = 1
+    connected = 0
+    while menu == 1:
+        print("Menu: ")
+        print("1. CONNECT <IP Address> <Server Port>")
+        print("2. LIST")
+        print("3. RETRIEVE <filename>")
+        print("4. STORE <filename>")
+        print("5. QUIT")
+        print("------------------------------")
 
-        # def store(file, fileSize, fileSizeSize):
-        #      #if fileSizeSize <= 1:
-        #      #s.send(b'0')
-        #      #s.sendall(bytes(str(fileSizeSize), 'utf8'))
-
-        # fileSize, fileSizeSize = headerEncoder("client.py")
-        # print("File Size Size: " + str(fileSizeSize))
-        # store("client.py", fileSize, fileSizeSize)
-        # print("This is what was sent " + str(sent))
-
-        menu = 1
-        while menu == 1:
-            print("Menu: ")
-            print("1. CONNECT <IP Address> <Server Port>")
-            print("2. LIST")
-            print("3. RETRIEVE <filename>")
-            print("4. STORE <filename>")
-            print("5. QUIT")
-            print("------------------------------")
-
-            response = input("Enter in your menu command: ").split()
-            if response[0] == "CONNECT":
-                connector(response[1], int(response[2]))
-            elif response[0] == "LIST":
-                list()
-            elif response[0] == "RETRIEVE":
-                retrieve(response[1])
-            elif response[0] == "STORE":
-                store(response[1])
-            elif response[0] == "QUIT":
-                menu = 0
-                s.close()
-            else:
-                print("NOT A VALID COMMAND! ")
+        response = input("Enter in your menu command: ").split()
+        if response[0] == "CONNECT":
+            connector(the_socket, response[1], int(response[2]))
+            connected = 1
+        elif response[0] == "LIST" and connected == 1:
+            list_receiver()
+        elif response[0] == "RETRIEVE" and connected == 1:
+            retrieve(response[1])
+        elif response[0] == "STORE" and connected == 1:
+            store(response[1])
+        elif response[0] == "QUIT":
+            menu = 0
+            quitTransaction()
+        elif connected == 0:
+            print("Server and Client are not connected! You must connect to the server first!")
+        else:
+            print("NOT A VALID COMMAND! ")
 
     #print('Received', repr(data))
     print("connect")
-    return s
+    return the_socket
+
+
+def connector(the_socket, IP, socketPort):
+    global HOST
+    global PORT
+    HOST = IP
+    PORT = socketPort
+    the_socket.connect((HOST, PORT))
+    the_socket.send(b'Connection Works')
 
 
 client()
+
+
